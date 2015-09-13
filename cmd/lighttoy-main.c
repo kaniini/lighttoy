b0e29a7a4aa9baf793395595918cbbb12aac975d
William Pitcock <nenolod@dereferenced.org>
2015-09-12 22:53:54 -0500

    lighttoy: add commands for setting colors

--- cmd/lighttoy-main.c
+++ cmd/lighttoy-main.c
@@ -23,7 +23,12 @@ struct libbulb_group root_group;
 #define PARAM_ON		2
 #define PARAM_OFF		3
 #define PARAM_PRINT             4
-#define PARAM_SIZE		5
+#define PARAM_HUE               5
+#define PARAM_SATURATION        6
+#define PARAM_VALUE             7
+#define PARAM_KELVIN            8
+#define PARAM_SETCOLOR          9
+#define PARAM_SIZE		10
 
 typedef void (*callback_func)(struct libbulb_light *light);
 
@@ -58,6 +63,15 @@ static void func_print(struct libbulb_light *light)
     printf("       State: %s\n", light->powered ? "ON" : "OFF");
 }
 
+static void func_hue(struct libbulb_light *light)        { light->color.hue = atof(optarg); }
+static void func_saturation(struct libbulb_light *light) { light->color.saturation = atof(optarg); }
+static void func_value(struct libbulb_light *light)      { light->color.value = atof(optarg); }
+static void func_kelvin(struct libbulb_light *light)     { light->color.kelvin = atof(optarg); }
+static void func_setcolor(struct libbulb_light *light) {
+    struct libbulb_color col = light->color;
+    libbulb_light_set_color(light, col);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -69,12 +83,22 @@ main(int argc, char *argv[])
         {"on", no_argument,          NULL, PARAM_ON},
         {"off", no_argument,         NULL, PARAM_OFF},
         {"info", no_argument,        NULL, PARAM_PRINT},
+        {"hue", required_argument,   NULL, PARAM_HUE},
+        {"saturation", required_argument, NULL, PARAM_SATURATION},
+        {"value", required_argument, NULL, PARAM_VALUE},
+        {"kelvin", required_argument, NULL, PARAM_KELVIN},
+        {"setcolor", no_argument,    NULL, PARAM_SETCOLOR},
     };
     static callback_func funcs[PARAM_SIZE] = {
         [PARAM_ON] = func_on,
         [PARAM_OFF] = func_off,
         [PARAM_TOGGLE] = func_toggle,
         [PARAM_PRINT] = func_print,
+        [PARAM_HUE] = func_hue,
+        [PARAM_SATURATION] = func_saturation,
+        [PARAM_VALUE] = func_value,
+        [PARAM_KELVIN] = func_kelvin,
+        [PARAM_SETCOLOR] = func_setcolor,
     };
 
     libbulb_group_discover(&root_group);
