6e53a7b976b359aae8cdda243a3dc52358a38b3d
William Pitcock <nenolod@dereferenced.org>
2015-09-12 22:00:57 -0500

    group-test: also dump the labels of lights detected in the group

--- tests/group-test.c
+++ tests/group-test.c
@@ -24,6 +24,7 @@ main(int argc, const char *argv[])
         printf("Light %d, address %0.2x%0.2x.%0.2x%0.2x.%0.2x%0.2x.%0.2x%0.2x:\n",
                ++i, light->address[0], light->address[1], light->address[2], light->address[3],
                light->address[4], light->address[5], light->address[6], light->address[7]);
+        printf("  Label: '%s'\n", light->label);
         printf("    Hue: %f\n", light->color.hue);
         printf("    Sat: %f\n", light->color.saturation);
         printf("    Val: %f\n", light->color.value);
