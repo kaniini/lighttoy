cb0e4eecfa139edf62908ea2adc34e3004f3fbfd
William Pitcock <nenolod@dereferenced.org>
2015-09-12 22:06:54 -0500

    cmd: add stub lighttoy command

---
+++ cmd/lighttoy-main.c
@@ -0,0 +1,22 @@
+/*
+ * lighttoy-main.c
+ * lighttoy - main()
+ *
+ * Copyright (c) 2015 William Pitcock <nenolod@dereferenced.org>.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * This software is provided 'as is' and without any warranty, express or
+ * implied.  In no event shall the authors be liable for any damages arising
+ * from the use of this software.
+ */
+
+#include <libbulb/libbulb.h>
+
+int
+main(int argc, const char *argv[])
+{
+    return 0;
+}
